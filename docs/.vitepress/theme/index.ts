4838b48f94b5685779dcddb49158ab0a8d48aa00
“MatNoble” <hustmatnoble@gmail.com>
2025-12-23 18:41:50 +0800

    feat: integrate @vercel/analytics 1.6.1 into VitePress theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,25 @@
-import { h } from 'vue'
+import { h, watch, onMounted } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import { useRoute } from 'vitepress'
 import Logo from './components/Logo.vue'
 import Comment from './components/Comment.vue'
 import DownloadCard from './components/DownloadCard.vue' // Import the new component
 import './custom.css'
+import { pageview } from '@vercel/analytics'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const route = useRoute()
+    onMounted(() => {
+      pageview()
+    })
+    watch(
+      () => route.path,
+      () => {
+        pageview()
+      }
+    )
     return h(DefaultTheme.Layout, null, {
       // 使用 nav-bar-title-before 插槽插入自定义 Logo
       'nav-bar-title-before': () => h(Logo),
